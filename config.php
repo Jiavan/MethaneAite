0acd1d7fad7c69b4c6ba6fede92b8f240eee3e42
Jiavan <mail@jiavan.com>
2015-07-23 20:31:20 +0800

    修改config文件，并删除methaneaite文件夹

--- config.php
+++ config.php
@@ -6,4 +6,13 @@
  * Time: 下午8:10
  */
 
+$config = array(
+    'dbconfig'=>array(
+        'dbhost'=>'127.0.0.1',
+        'dbuser'=>'root',
+        'dbpsw'=>'cfc2013114',
+        'dbname'=>'methaneaite',
+        'dbcharset'=>'utf8'
+    )
+);
 ?>
